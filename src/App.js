76e7e30ab157d399d3261e510cbdd05da6714d63
amrutsabale <samrut355@gmail.com>
2021-12-19 17:04:24 +0530

    Created react onesignal notification poc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import OneSignal from "react-onesignal";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    OneSignal.init({
+      appId: "d6a64b71-5085-4e1f-bb5a-7d8140f9be63",
+    });
+
+    OneSignal.addListenerForNotificationOpened(() => {
+      console.log("hi");
+    });
+
+    OneSignal.on("addListenerForNotificationOpened", function (event) {
+      console.log("Notification Clicked", event);
+    });
+
+    OneSignal.on("notificationDisplay", function (event) {
+      console.log("OneSignal notification displayed:", event);
+    });
+    OneSignal.on("notificationDismiss", function (event) {
+      console.log("OneSignal notification dismissed:", event);
+    });
+  }, []);
+
+  const onHandleTag = (tag) => {
+    console.log("Tagging");
+    OneSignal.sendTag("tech", tag).then(() => {
+      console.log("tagged");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> React one signal push notification demo</h1>
+      <button onClick={() => onHandleTag("react")}>Click</button>
     </div>
   );
 }
